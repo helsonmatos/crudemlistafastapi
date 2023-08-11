64927d7d7543ddd64dc2b4899a1cdae15d83932b
Helson Matos <77506662+helsonmatos@users.noreply.github.com>
2023-08-11 10:04:18 -0300

    Adicionando código python

---
+++ crud_listas.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+
+app = FastAPI()
+
+class Usuario(BaseModel):
+    id: int
+    nome: str
+    senha: str
+
+
+
+lista = [
+    Usuario(id=1, nome='neymar', senha='senha1'),
+    Usuario(id=2, nome='ronaldo', senha='senha2'),
+    Usuario(id=3, nome='romario', senha='senha3'),
+    Usuario(id=4, nome='rivaldo', senha='senha4'),
+    Usuario(id=5, nome='kaka', senha='senha5'),
+]
+
+@app.post('/usuario')
+def main(usuario: Usuario):
+    lista.append(usuario)
+    return usuario
+
+@app.get('/usuarioListar')
+def main():
+    return lista
